2753c8ee87c93c163465bf3844ee0bedb027152c
Iaarun <automationbyaarun@gmail.com>
2022-12-19 22:00:31 +0530

    adding senddata code

--- src/seleniumScripts/SeleniumScripts.java
+++ src/seleniumScripts/SeleniumScripts.java
@@ -1,8 +1,11 @@
 package seleniumScripts;
 
+import java.util.List;
 import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -15,8 +18,46 @@ public class SeleniumScripts {
 	public static void main(String[] args) {
 		SeleniumScripts ss = new SeleniumScripts();
 		ss.launchBrowsers("firefox");
-		ss.basicfunction();
-		ss.closeBrowser();
+		ss.handleMultipleCheckbox();
+		// ss.closeBrowser();
+	}
+
+	public void handleMultipleCheckbox() {
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+		List<WebElement> checkboxes = driver.findElements(By.name("my-check"));
+		
+		checkboxes.forEach(x->{
+			if(!x.isSelected()) {
+				x.click();
+			}
+		});
+	}
+
+	public void sendDataCheckboxRadiobutton() {
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+		WebElement userinput = driver.findElement(By.id("my-text-id"));
+		userinput.sendKeys("Selenium Practice");
+
+		WebElement checkbox2 = driver.findElement(By.id("my-check-2"));
+		checkbox2.click();
+
+		WebElement checkbox1 = driver.findElement(By.id("my-check-1"));
+		checkbox1.click();
+
+	}
+
+	public void navigationcommands() {
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/index.html");
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().to("https://www.facebook.com/");
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().back();
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().forward();
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().refresh();
+
 	}
 
 	public void basicfunction() {
@@ -29,15 +70,17 @@ public class SeleniumScripts {
 
 		String tabId = driver.getWindowHandle();
 		System.out.println(tabId);
-		
-		Set<String> alltabs= driver.getWindowHandles();
-		
-		alltabs.forEach(x->{System.out.println(x);});
-		
+
+		Set<String> alltabs = driver.getWindowHandles();
+
+		alltabs.forEach(x -> {
+			System.out.println(x);
+		});
+
 //		for(String x: alltabs) {
 //			System.out.println(x);
 //		}
-		
+
 		System.out.println(driver.getPageSource());
 	}
 
